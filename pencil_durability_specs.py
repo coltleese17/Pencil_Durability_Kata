3b72eb53823a7e72324427b38dc06dd51e0e120f
ColtonLeese <coltleese@gmail.com>
2018-01-06 17:42:59 -0500

    inital test setup confirmed and main class file added

---
+++ pencil_durability_specs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import unittest
+import pencil_durability as pd 
+
+class PencilDurabilityTests(unittest.TestCase): 
+
+	def test_truth(self):
+		self.assertTrue(True)
+
+if __name__ == '__main__':
+	unittest.main()
